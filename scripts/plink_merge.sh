93df7a04428bf267f20d7886e0607014a6576895
XinAlon <159056448+XinAlon@users.noreply.github.com>
2024-09-11 13:55:46 +0200

    Create plink_merge.sh

---
+++ scripts/plink_merge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Directory containing the files
+DIR="$(pwd)"
+
+merge_file="$DIR/merge_list.txt"
+> merge_file
+
+for file in "$DIR"/*.bed; do
+    if [ -f "$file" ]; then
+        base_file="${file%.bed}"  # Remove the .bed extension
+        echo "$base_file" >> "$merge_file"
+    fi
+done
+
+if [ -f "$merge_file" ]; then
+    echo "Running plink --bmerge with $merge_file"
+    ./plink --merge-list "$merge_file" --out "$DIR/merged_output"
+else
+    echo "List file $merge_file does not exist. No files to merge."
+fi
+
+echo "All files have been merged"
